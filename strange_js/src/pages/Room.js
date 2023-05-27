9b18c671e83e22d988b993e6a47086cd921b3b1a
Ahn Hyesu <linim@naver.com>
2023-05-27 22:20:29 +0900

    1.방에서 나갔을 때 room_number: 0, joined_room: false 로 변경. -> 서버에서는 안나가지는거같은데 확인 1-1. store에서 storeLeaveGame,LEAVE_GAME 추가. 2. 만들어진 방으로 이동하도록 변경 (나중에 수정해야함)

--- strange_js/src/pages/Room.js
+++ strange_js/src/pages/Room.js
@@ -1,47 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 import io from "socket.io-client";
-import { actionCreators } from '../store';
-
+import { actionCreators } from "../store";
 
 const socket = io.connect("http://localhost:5000");
 
-const Room = ({props, storeMakeID, storeJoinGame}) => {
+const Room = ({ props, storeMakeID, storeJoinGame, storeLeaveGame }) => {
+	const navigate = useNavigate(); //페이지 주소 전달
 
-    const navigate = useNavigate(); //페이지 주소 전달 
+	const navigateBack = () => {
+		navigate("../", { replace: true });
+		storeLeaveGame(0);
+	};
 
-    const navigateBack = () => {
-        navigate('../', { replace: true });
-        storeJoinGame(0);
-    };
-
-    return (
-
-        <div>
-            <Button variant="primary" type="submit" onClick={navigateBack}>
-                방 나가기
-            </Button>
-            <h1>{props.room_number}번방 입니다.</h1>
-            <h2>접속자</h2>
-            <p>1. {props.id}</p>
-            <p>플레이어2</p>
-        </div>
-    );
+	return (
+		<div>
+			<Button variant='primary' type='submit' onClick={navigateBack}>
+				방 나가기
+			</Button>
+			<h1>{props.room_number}번방 입니다.</h1>
+			<h2>접속자</h2>
+			<p>1. {props.id}</p>
+			<p>플레이어2</p>
+		</div>
+	);
 };
 
-function mapStateToProps(state){
-    console.log('state:',state)
-    return { props: state };
+function mapStateToProps(state) {
+	console.log("state:", state);
+	return { props: state };
 }
-function mapDispatchToProps(dispatch){
-    return {
-        storeMakeID: id => dispatch(actionCreators.storeMakeID(id)),
-        storeJoinGame: room_number => dispatch(actionCreators.storeJoinGame(room_number)),
-        storeMakeGame: (room_number )=> dispatch(actionCreators.storeMakeGame(room_number)), 
-    };
+function mapDispatchToProps(dispatch) {
+	return {
+		storeMakeID: (id) => dispatch(actionCreators.storeMakeID(id)),
+		storeJoinGame: (room_number) =>
+			dispatch(actionCreators.storeJoinGame(room_number)),
+		storeMakeGame: (room_number) =>
+			dispatch(actionCreators.storeMakeGame(room_number)),
+		storeLeaveGame: (room_number) =>
+			dispatch(actionCreators.storeLeaveGame(room_number)),
+	};
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Room);
